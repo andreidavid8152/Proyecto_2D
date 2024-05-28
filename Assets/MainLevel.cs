b3ff37933f11f8eac5a4e6357ebaeacc7b811729
andreidavid8152 <andreidavid3399@gmail.com>
2024-05-27 20:30:45 -0500

    Main menu - niveles

---
+++ Assets/MainLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainLevel : MonoBehaviour
+{
+    public void Volver()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Borrar()
+    {
+
+    }
+}
