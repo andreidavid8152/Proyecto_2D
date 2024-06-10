54c992f6a3363d137113ee4f16ecac782e7b4210
andreidavid8152 <andreidavid3399@gmail.com>
2024-06-10 16:55:52 -0500

    ultrashoot

---
+++ Assets/Scripts/ultrashoot.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ultrashoot : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D Rigidbody2D;
+    private Vector2 Direction;
+    public int damage = 3; // Cantidad de daño que la bala inflige
+
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Rigidbody2D.velocity = Direction * speed;
+    }
+
+    public void setDirection(Vector2 direction)
+    {
+        Direction = direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy") && gameObject.CompareTag("PlayerBullet"))
+        {
+
+            Enemigo enemigo = other.GetComponent<Enemigo>();
+            if (enemigo != null)
+            {
+                GameManager.Instance.PerderVidaEnemigo(damage); // Aplicar daño al jugador
+                enemigo.RecibirDanio(damage); // Aplicar daño al enemigo
+                Destroy(gameObject); // Destruir la bala al impactar
+            }
+        }
+    }
+
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
